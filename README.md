a1e1e72704bfc8194703f21e1be8009266c72cec
neststack <nsitseo@gmail.com>
2022-08-19 19:32:50 +0530

    edit README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A simple project to practise firebase authentication with email and password. Also made use of the useContext hook to pass the authentication state across app.
 
+You will need to add your 'firebaseConfig' in src/firebase.js to make this work.
+
 Thank you :)
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+
+//////////////////////////////////////////////
+/* Please add your firebaseConfig settings to make this work
+They'll look like this>
+
+  const firebaseConfig = {
+    apiKey: 'adadsada2dav',
+    authDomain: '*****.firebaseapp.com',
+    projectId: 'ffakjndfjkaf',
+    storageBucket: 'asgadahdhad',
+    messagingSenderId: 'ahdhadfa',
+    appId: 'afhadfsfasfs',
+  };
+*/
 import { firebaseConfig } from './firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
